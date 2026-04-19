ac11b4f64bb406177dffd1776c86e98a107d9704
Fernando <medeirosfernandoc@gmail.com>
2026-04-19 11:46:24 -0300

    correção test_004

--- tests/test_004_verificar_emprestimo_contratado.py
+++ tests/test_004_verificar_emprestimo_contratado.py
@@ -1,5 +1,5 @@
 def test_004_verificar_emprestimo_contratado(login_page, emprestimos_page, home_page, common_page):
-    login_page.login("user1", "pass1")
+    login_page.login("user2", "pass2")
     login_page.assert_login_successful()
     home_page.acessar_menu("Empréstimos")
     common_page.assert_text("Empréstimos - Empréstimos Pré-aprovados")
@@ -7,7 +7,7 @@ def test_004_verificar_emprestimo_contratado(login_page, emprestimos_page, home_
     emprestimos_page.clicar_contratar_emprestimo()
     common_page.assert_text("A transação foi concluída com sucesso. Você pode voltar para a página principal e continuar suas operações.")
     common_page.voltar_para_home()
-    common_page.assert_text("Saldo atual: R$ 7.000,00")
+    common_page.assert_text("Saldo atual: R$ 2.300,00")
     home_page.acessar_menu("Empréstimos")
     common_page.assert_text("Você já tem um empréstimo contratado em andamento. Novos empréstimos não estão disponíveis.")
 
